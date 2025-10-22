99dc64e4cc9a4803d2ececaddde6430e6e8ca1ed
Mustafa Sayed <mustafacoder9@gmail.com>
2025-10-22 03:41:08 +0300

    Add Breadcrumb component to MainContent

--- src/Components/MainContent/index.jsx
+++ src/Components/MainContent/index.jsx
@@ -1,11 +1,16 @@
+import Breadcrumb from "@components/Breadcrumb";
 import Header from "./components/Header";
 
 function MainContent({ children }) {
     return (
         <main className="main-content bg-[#F1F5F9] text-[#212b36] w-full max-h-full overflow-y-auto">
+            {/* Header */}
             <Header />
             {/* Page */}
             <div className="page-wrapper p-3 sm:p-5 md:p-7 2xl:p-10">
+                {/* Breadcrumb */}
+                <Breadcrumb className="mb-5" />
+                {/* ...Page Here... */}
                 {
                     children
                 }
